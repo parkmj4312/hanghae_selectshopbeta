1956a19c8fe33e83c567ff10a59a55d8e13625a5
parker4942 <parkmj4312@gmail.com>
2023-03-04 18:25:09 +0900

    셀렉트샵 만들기 연습 베타 완성 커밋

--- src/main/java/com/sparta/myselectshopbeta/controller/ShopController.java
+++ src/main/java/com/sparta/myselectshopbeta/controller/ShopController.java
@@ -1,4 +1,4 @@
-package com.sparta.myselectshopbeta.naver.controller;
+package com.sparta.myselectshopbeta.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
